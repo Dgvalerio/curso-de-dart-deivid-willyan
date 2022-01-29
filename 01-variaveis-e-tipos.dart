1c20c6a47495195badaab23474dd73d8094660a4
Dgvalerio <al.davigvalerio@gmail.com>
2022-01-29 19:57:37 -0300

    docs: 01 - Introdução a Variáveis

---
+++ 01-variaveis-e-tipos.dart
@@ -0,0 +1,16 @@
+// 01 - Introdução a Variáveis
+void main() {
+  String nome = "Davi";
+  print(nome);
+
+  int idade = 21;
+  print(idade);
+
+  bool eHomem = true;
+  print(eHomem);
+
+  List<String> nomeCompleto = ["Davi", "Gonçalves", "Valério"];
+  print(nomeCompleto);
+  print(nomeCompleto[0]);
+  print('${nomeCompleto[0]} ${nomeCompleto[1]} ${nomeCompleto[2]}');
+}
